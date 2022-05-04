9d741fbe34333c85414036441fa986ba8020f9c1
Lateef Quadri Olayinka <lateef9816@gmail.com>
2022-05-04 08:21:06 +0100

    notes page IP

--- src/pages/notes/notes.tsx
+++ src/pages/notes/notes.tsx
@@ -1,7 +1,29 @@
+import { Box, Button, Flex, VStack } from "@chakra-ui/react";
 import Header from "../../components/header/header";
-
+import { MdPlusOne } from "react-icons/md";
 const Notes = () => {
-  return <Header>Notes</Header>;
+  return (
+    <VStack w="full" h="100vh">
+      <Header>Notes</Header>
+
+      <Flex w="full" h="100%">
+        <Box flex={2} h="full" bg="gray.100" py={15}>
+          <Button
+            py={7}
+            px={10}
+            variant="outline"
+            colorScheme="blackAlpha"
+            borderTopLeftRadius={30}
+            borderBottomRightRadius={30}
+            leftIcon={<MdPlusOne />}
+          >
+            Create Note
+          </Button>
+        </Box>
+        <Box flex={9} h="full" bg="gray.100"></Box>
+      </Flex>
+    </VStack>
+  );
 };
 
 export default Notes;
